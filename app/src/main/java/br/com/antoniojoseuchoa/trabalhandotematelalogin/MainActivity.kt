b8ab48e1931795b2b810f5f114645a616421c2a2
devmobile <antoniojoseuchoa2020@gmail.com>
2022-12-08 09:18:53 -0300

    Commit criando a tela de login, implementando o binding e configurando os temas.

--- app/src/main/java/br/com/antoniojoseuchoa/trabalhandotematelalogin/MainActivity.kt
+++ app/src/main/java/br/com/antoniojoseuchoa/trabalhandotematelalogin/MainActivity.kt
@@ -2,10 +2,23 @@ package br.com.antoniojoseuchoa.trabalhandotematelalogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import br.com.antoniojoseuchoa.trabalhandotematelalogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        listeners()
+    }
+
+    fun listeners(){
+        binding.switch1.setOnCheckedChangeListener { compoundButton, isChecked ->
+              if(isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
